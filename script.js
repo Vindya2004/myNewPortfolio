60f45f7be3359a172f0803dcb32bb6c34467be00
Vindya2004 <vindyamadubhashini@gmail.com>
2025-05-06 19:57:36 +0530

    add js

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
+// let menu = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+//
+// menu.onclick = () => {
+//     menu.classList.toggle('bx-x');
+//     navbar.classList.toggle('active');
+// }
+// window.onscroll = () => {
+//     menu.classList.remove('bx-x');
+//     navbar.classList.remove('active');
+// }
+// ScrollReveal({
+//     distance: '80px',
+//     duration:2000,
+//     delay:200,
+// });
+// ScrollReveal().reveal('.home-content,heading,.skills h1,.assignment h1,.services h1,.projects h1',{origin: 'top'});
+// ScrollReveal().reveal('.home-img, .portfolio-box, .contact form,.container,.project-container' ,{origin: 'button'});
+// ScrollReveal().reveal('.home-content h1, .about-img,.services-container', {origin: 'left'});
+// ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
+// ScrollReveal().reveal('.footer',{origin:'bottom'});
+// const typed = new Typed('.multiple-text', {
+//     strings: ['Frontend Developer', 'Backend Developer' , 'Web Developer'],
+//     typeSpeed: 80,
+//     backSpeed: 80,
+//     backDelay:1200,
+//     loop:true,
+// });
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
+    menu.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 }
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
+    menu.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 }
+
+// ScrollReveal
 ScrollReveal({
     distance: '80px',
-    duration:2000,
-    delay:200,
+    duration: 2000,
+    delay: 200,
 });
-ScrollReveal().reveal('.home-content,heading,.skills h1,.assignment h1,.services h1,.projects h1',{origin: 'top'});
-ScrollReveal().reveal('.home-img, .portfolio-box, .contact form,.container,.project-container' ,{origin: 'button'});
-ScrollReveal().reveal('.home-content h1, .about-img,.services-container', {origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
-ScrollReveal().reveal('.footer',{origin:'bottom'});
+ScrollReveal().reveal('.home-content, heading, .skills h1, .assignment h1, .services h1, .projects h1', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form, .container, .project-container', {origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img, .services-container', {origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+ScrollReveal().reveal('.footer', {origin: 'bottom'});
+
+// Typed JS
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Backend Developer' , 'Web Developer'],
+    strings: ['Frontend Developer', 'Backend Developer', 'Web Developer'],
     typeSpeed: 80,
     backSpeed: 80,
-    backDelay:1200,
-    loop:true,
+    backDelay: 1200,
+    loop: true,
 });
